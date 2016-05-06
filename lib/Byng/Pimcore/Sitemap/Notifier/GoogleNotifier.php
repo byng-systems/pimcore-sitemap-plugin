16bf606d48c4bfd1ebfed39f0f48b8e96eb503c2
Elliot Wright <wright.elliot@gmail.com>
2016-05-06 11:45:11 +0100

    Made code style, and documentation consistent with other Byng Pimcore OSS.

--- lib/Byng/Pimcore/Sitemap/Notifier/GoogleNotifier.php
+++ lib/Byng/Pimcore/Sitemap/Notifier/GoogleNotifier.php
@@ -1,22 +1,31 @@
 <?php
 
+/**
+ * This file is part of the pimcore-sitemap-plugin package.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Byng\Pimcore\Sitemap\Notifier;
 
 use Pimcore\Config;
-use Byng\Pimcore\Sitemap\Notifier\Service\SitemapNotifierInterface;
 
 /**
- * Class GoogleNotifier
+ * Google Notifier
  *
- * @package PimcoreSitemapPlugin\Notifier
+ * @author Ioannis Giakoumidis <ioannis@byng.co>
  */
-class GoogleNotifier implements SitemapNotifierInterface
+final class GoogleNotifier implements SitemapNotifierInterface
 {
     /**
-     * Notify Google for new sitemap
-     *
-     * @return bool
+     * {@inheritdoc}
      */
     public function notify()
     {
@@ -26,17 +35,21 @@ class GoogleNotifier implements SitemapNotifierInterface
                 Config::getSystemConfig()->get("general")->get("domain")
             )
         );
+
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         $data = curl_exec($ch);
+
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
         curl_close($ch);
+
         if ($httpcode >= 200 && $httpcode < 300){
             return true;
         } else {
             return false;
         }
     }
-
 }
